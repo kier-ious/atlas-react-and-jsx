eb8a90ce55dce99d5ed656b632119f96935fbd53
kier-ious <kier.diemer@gmail.com>
2024-08-26 11:31:15 -0500

    Initialized and rendered React App, task 0

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import "./index.css";
-// adding something
+
+
 // Render the App component to the root element
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
